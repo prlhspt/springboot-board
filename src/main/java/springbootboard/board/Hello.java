571c7b83cddd260448629a1ce28de6e7fde09ded
prlhspt <prlhspt@gmail.com>
2022-05-13 00:40:01 +0900

    First Commit

---
+++ src/main/java/springbootboard/board/Hello.java
@@ -0,0 +1,18 @@
+package springbootboard.board;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Hello {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+
+
+}
---
+++ src/main/java/springbootboard/board/config/auth/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package springbootboard.board.config.auth;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import springbootboard.board.domain.Role;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/hello")
+                .permitAll()
+                .antMatchers("/admin").hasAnyRole(Role.ADMIN.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
+
+    }
+}
