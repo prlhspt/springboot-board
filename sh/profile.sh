10787c7f33c960ac1725b742888a6ae92ad74998
임현석 <prlhspt@gmail.com>
2022-06-21 19:13:35 +0900

    Fix: 배포 스크립트 sudo 사용하도록 수정

--- sh/profile.sh
+++ sh/profile.sh
@@ -2,16 +2,16 @@
 
 function find_idle_profile()
 {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    RESPONSE_CODE=$(curl -s --insecure -o /dev/null -w "%{http_code}" https://localhost/profile)
 
     if [ "${RESPONSE_CODE}" -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=real2
     else
-        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+        CURRENT_PROFILE=$(curl -s --insecure https://localhost/profile)
     fi
 
-    if [ ${CURRENT_PROFILE} == real1 ]
+    if [ "${CURRENT_PROFILE}" == real1 ]
     then
       IDLE_PROFILE=real2
     else
